c1fb31be147a770c65fe85da89d0fc40903d10f6
Luka Devic <luka.m.devic@gmail.com>
2018-10-09 15:34:25 +0200

    added consumtion of API for users data

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import CardList from './CardList';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 
